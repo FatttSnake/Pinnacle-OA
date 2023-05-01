0d7f82531dc87c0f7a9a00a46a32324884ef1251
FatttSnake <fatttsnake@gmail.com>
2023-05-01 19:52:10 +0800

    Fixed eslint

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -1,19 +1,27 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
   },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "standard-with-typescript",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting"
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    project: "./tsconfig*.json"
+  },
+  plugins: [
+    "vue"
+  ],
   rules: {
     "no-cond-assign": "error",
     "indent": ["error", 4]
   }
-}
+};
