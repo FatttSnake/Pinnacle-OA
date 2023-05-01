836fb1561afb8947f778fd549285f19e20a062a0
FatttSnake <fatttsnake@gmail.com>
2023-05-02 00:00:29 +0800

    Fixed eslint

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -1,5 +1,4 @@
 module.exports = {
-  root: true,
   env: {
     browser: true,
     es2021: true,
@@ -7,21 +6,28 @@ module.exports = {
   },
   extends: [
     "plugin:vue/vue3-essential",
-    "eslint:recommended",
     "standard-with-typescript",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier/skip-formatting"
+    "plugin:prettier/recommended"
   ],
   overrides: [],
   parserOptions: {
+    project: "./tsconfig*.json",
     ecmaVersion: "latest",
-    project: "./tsconfig*.json"
+    sourceType: "module"
   },
   plugins: [
-    "vue"
+    "vue",
+    "prettier"
   ],
   rules: {
     "no-cond-assign": "error",
-    "indent": ["error", 4]
+    "eqeqeq": "error",
+    "indent": ["error", 4],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ]
   }
 };
