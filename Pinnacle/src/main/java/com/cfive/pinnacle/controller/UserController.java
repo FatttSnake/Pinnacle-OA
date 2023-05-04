7695a20e776051a82a12ed0bcbce065ba65cbc4e
FatttSnake <fatttsnake@gmail.com>
2023-05-05 01:12:44 +0800

    Fixed jwt verify. Blocked access to the password for the user controller.

--- Pinnacle/src/main/java/com/cfive/pinnacle/controller/UserController.java
+++ Pinnacle/src/main/java/com/cfive/pinnacle/controller/UserController.java
@@ -6,6 +6,7 @@ import com.cfive.pinnacle.entity.common.ResponseResult;
 import com.cfive.pinnacle.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,7 +32,13 @@ public class UserController {
 
     @GetMapping
     public ResponseResult getAllUser() {
-        List<User> users = userService.list();
+        List<User> users = userService.getBasicInfo();
         return ResponseResult.build(ResponseCode.DATABASE_SELECT_OK, "success", users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseResult getUser(@PathVariable int id) {
+        User user = userService.getBasicInfo(id);
+        return ResponseResult.build(ResponseCode.DATABASE_SELECT_OK, "success", user);
     }
 }
--- Pinnacle/src/main/java/com/cfive/pinnacle/entity/permission/LoginUser.java
+++ Pinnacle/src/main/java/com/cfive/pinnacle/entity/permission/LoginUser.java
@@ -17,47 +17,99 @@ public class LoginUser implements UserDetails {
     private Collection<? extends GrantedAuthority> authorities;
     private String password;
     private String username;
-    private Boolean accountNonExpired;
-    private Boolean accountNonLocked;
-    private Boolean credentialsNonExpired;
-    private Boolean enabled;
+    private Boolean accountNonExpired = true;
+    private Boolean accountNonLocked = true;
+    private Boolean credentialsNonExpired = true;
+    private Boolean enabled = true;
 
     public LoginUser(User user) {
         this.user = user;
+        this.username = user.getUsername();
+        this.password = user.getPasswd();
+        this.enabled = user.getEnable() == 1;
     }
 
-    @Override
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
-    @Override
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
     public String getPassword() {
-        return user.getPasswd();
+        return password;
     }
 
-    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getUsername() {
-        return user.getUsername();
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+        return this.accountNonExpired;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+        return this.accountNonLocked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return this.credentialsNonExpired;
     }
 
     @Override
     public boolean isEnabled() {
-        return user.getEnable() == 1;
+        return this.enabled;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Boolean getAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    public void setAccountNonExpired(Boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    public Boolean getAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public void setAccountNonLocked(Boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public Boolean getCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    public void setCredentialsNonExpired(Boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
     }
 }
