2fa80301e25547beed63f6d64d546046a34e7621
gzw <1571020389@qq.com>
2023-05-06 00:41:06 +0800

    commit 2023/05/06

--- Pinnacle/src/main/java/com/cfive/pinnacle/entity/Attendance.java
+++ Pinnacle/src/main/java/com/cfive/pinnacle/entity/Attendance.java
@@ -10,6 +10,9 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
@@ -18,7 +21,7 @@ import lombok.experimental.Accessors;
  * 考勤
  * </p>
  *
- * @author FatttSnake
+ * @author gzw
  * @since 2023-04-30
  */
 @Data
@@ -30,18 +33,21 @@ public class Attendance implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @TableId("id")
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
     /**
      * 用户
      */
     @TableField("user_id")
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long userId;
 
     /**
      * 考勤时间
      */
     @TableField("att_time")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",timezone = "UTC")
     private LocalDateTime attTime;
 
     /**
@@ -54,6 +60,7 @@ public class Attendance implements Serializable {
      * 修改人
      */
     @TableField("modify_id")
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long modifyId;
 
     /**
@@ -63,10 +70,13 @@ public class Attendance implements Serializable {
     private LocalDateTime modifyTime;
 
     @TableField("deleted")
-    @TableLogic
+    @TableLogic(value = "0",delval = "1")
     private Integer deleted;
 
     @TableField("version")
     @Version
     private Integer version;
+
+    @TableField(exist = false)
+    private User user;
 }
--- Pinnacle/src/main/java/com/cfive/pinnacle/service/IAttendanceService.java
+++ Pinnacle/src/main/java/com/cfive/pinnacle/service/IAttendanceService.java
@@ -1,16 +1,26 @@
 package com.cfive.pinnacle.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.cfive.pinnacle.entity.Attendance;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
  * 考勤 服务类
  * </p>
  *
- * @author FatttSnake
+ * @author gzw
  * @since 2023-04-30
  */
 public interface IAttendanceService extends IService<Attendance> {
+    List<Attendance> getAllAttendanceAndUser();
+    List<Attendance> getAttendanceAndUserByid(Long userId);
+
+
+    List<Attendance> selectByTime(String startTime,String endTime);
+    List<Attendance> selectOneByTime(String startTime,String endTime,Long userId);
+
 
 }
